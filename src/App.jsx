1636513e88e5abd8737bec4f84d411a9760e28f3
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-09-02 19:18:14 +0530

    adding header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,27 @@ import Contact from "./pages/Contact";
 import LeaderBoard from "./pages/LeaderBoard";
 import Blog from "./pages/Blog";
 import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/events" element={<Events />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="/leaderboard" element={<LeaderBoard />} />
-				<Route path="/blog" element={<Blog />} />
-				<Route path="/*" element={<NotFound />} />
-			</Routes>
-		</Router>
+		<div className="app">
+			<Header />
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/events" element={<Events />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/leaderboard" element={<LeaderBoard />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/*" element={<NotFound />} />
+				</Routes>
+			</Router>
+			<Footer />
+		</div>
 	);
 }
 
