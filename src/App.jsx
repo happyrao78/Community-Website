119760b8db036094bbdbc09d362220cb7f8db5b1
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-09-02 18:09:55 +0530

    proper structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1 className="font-bold text-3xl bg-slate-400 rounded-lg mx-auto p-2">
+				365 Days Coding Tech Community
+			</h1>
+		</>
+	);
+}
+
+export default App;
